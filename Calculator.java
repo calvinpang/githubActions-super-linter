830740cf980fdbaf3b97b4ca396e7d90d2af1672
Calvin Pang <calvin.pang@gmail.com>
2025-06-26 12:48:57 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,12 +6,12 @@ package com.example;
 public class Calculator {
 
     /** Adds two integers. */
-    public int add(int a, int b) {    // add method
+    public int add(int a, int b) { // add method
         return a + b;
     }
 
     /** Subtracts b from a. */
-    public int subtract(int a, int b) {    // subtract method
+    public int subtract(int a, int b) { // subtract method
         return a - b;
     }
 }
